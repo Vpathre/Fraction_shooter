aa6347ba8b746fdcf0cca0e0ee80209daae36fa3
Viren Pathre <pathreviren24@gmail.com>
2022-08-02 15:06:47 +0200

    First Commit

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 Author: Viren Pathre (Resolute Education)
 Date:22/7/22*/
 
+var questions_array = [];
+
 var str =
     'function generate(){\n//add code functionality\n}';
 
@@ -29,6 +31,61 @@ class Ball {
     }
 }
 
+class Questions {
+    constructor(question, answer, asked) {
+        this.question = question;
+        this.answer = answer;
+        this.asked = asked;
+    }
+
+
+    getRandomQuestion() {
+        let send = questions_array[Math.floor(Math.random() * (questions_array.length - 1 - 0 + 1) + 0)];
+        while (send.asked == true) {
+            send = questions_array[Math.floor(Math.random() * (questions_array.length - 1 - 0 + 1) + 0)];
+        }
+        return [send.question, send.answer];
+    }
+}
+
+// populate array with questions
+function initializeArray() {
+    for (let i = 0; i < 200; i++) {
+        let temp = fraction_randomizer()
+        questions_array.push(new Questions(temp[0], temp[1], false));
+    }
+}
+
+// try to generate random question string and answer
+function fraction_randomizer() {
+    let num1 = (Math.floor(Math.random() * (199 - 1 + 1) + 1));
+    let den1 = (Math.floor(Math.random() * (199 - 1 + 1) + 1));
+    let num2 = (Math.floor(Math.random() * (199 - 1 + 1) + 1));
+    let den2 = (Math.floor(Math.random() * (199 - 1 + 1) + 1));
+    let operator;
+
+    switch (Math.floor(Math.random() * (4 - 1 + 1) + 1)) {
+        case 1:
+            operator = "+";
+            break;
+        case 2:
+            operator = "-";
+            break;
+        case 3:
+            operator = "*";
+            break;
+        case 4:
+            operator = "/";
+            break;
+    }
+
+    let ques = num1 + "/" + den1 + operator + num2 + "/" + den2;
+    let ans = eval(ques);
+    return [ques, ans];
+}
+
+initializeArray();
+
 //set configuration for the scene
 var config = {
     type: Phaser.AUTO,
@@ -49,7 +106,12 @@ var config = {
             }
         }
     },
-    scene: [Level_1, Level_2, Level_3, Level_4, Level_5]
+    scene: [Level_1,
+        Level_2,
+        Level_3,
+        Level_4,
+        Level_5
+    ]
 };
 
 //start new game scene
